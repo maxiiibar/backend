0252d3ec9376a08698063c1b5ade9cf65bde8793
Maximo Bar <maximobar2003@gmail.com>
2024-04-30 21:18:48 -0300

    Desafio 3

---
+++ products.js
@@ -0,0 +1,92 @@
+export const products = [
+  {
+    id: 1,
+    title: "Smartphone XYZ",
+    description: "Smartphone de alta gama con pantalla OLED",
+    price: 100000,
+    thumbnail: "Sin imagen",
+    code: "tech001",
+    stock: 50
+  },
+  {
+    id: 2,
+    title: "Laptop ABC",
+    description: "Laptop ultradelgada con procesador de última generación",
+    price: 150000,
+    thumbnail: "Sin imagen",
+    code: "tech002",
+    stock: 30
+  },
+  {
+    id: 3,
+    title: "Cámara DSLR 123",
+    description: "Cámara réflex digital con sensor de alta resolución",
+    price: 80000,
+    thumbnail: "Sin imagen",
+    code: "tech003",
+    stock: 20
+  },
+  {
+    id: 4,
+    title: "Tablet XYZ",
+    description: "Tablet con pantalla retina y lápiz óptico incluido",
+    price: 70000,
+    thumbnail: "Sin imagen",
+    code: "tech004",
+    stock: 40
+  },
+  {
+    id: 5,
+    title: "Auriculares ABC",
+    description: "Auriculares inalámbricos con cancelación de ruido",
+    price: 50000,
+    thumbnail: "Sin imagen",
+    code: "tech005",
+    stock: 60
+  },
+  {
+    id: 6,
+    title: "Smartwatch XYZ",
+    description: "Smartwatch con monitor de actividad y GPS integrado",
+    price: 60000,
+    thumbnail: "Sin imagen",
+    code: "tech006",
+    stock: 35
+  },
+  {
+    id: 7,
+    title: "Impresora ABC",
+    description: "Impresora multifunción con conexión WiFi",
+    price: 90000,
+    thumbnail: "Sin imagen",
+    code: "tech007",
+    stock: 25
+  },
+  {
+    id: 8,
+    title: "Monitor XYZ",
+    description: "Monitor LED de 27 pulgadas con resolución 4K",
+    price: 120000,
+    thumbnail: "Sin imagen",
+    code: "tech008",
+    stock: 15
+  },
+  {
+    id: 9,
+    title: "Router ABC",
+    description: "Router de alta velocidad compatible con estándares WiFi 6",
+    price: 80000,
+    thumbnail: "Sin imagen",
+    code: "tech009",
+    stock: 20
+  },
+  {
+    id: 10,
+    title: "Altavoz XYZ",
+    description: "Altavoz inteligente con asistente de voz integrado",
+    price: 70000,
+    thumbnail: "Sin imagen",
+    code: "tech010",
+    stock: 30
+  }
+];
