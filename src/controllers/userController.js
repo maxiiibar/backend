07494891dd0fbd49773c61d8f0ff1909539bd2eb
Maximo Bar <maximobar2003@gmail.com>
2024-09-04 21:45:17 -0300

    Funcionalidad de reseteo de la password casi terminada

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,7 @@ import Controller from "./classController.js";
 import UserServices from "../services/userServices.js";
 const userService = new UserServices();
 import HttpResponse from "../utils/httpResponse.js";
+import { token } from "morgan";
 const httpResponse = new HttpResponse();
 
 export default class UserController extends Controller {
@@ -26,18 +27,18 @@ export default class UserController extends Controller {
   loginResponse = async (req, res, next) => {
     try {
       const token = await this.service.login(req.body);
-      if (!token) httpResponse.Unauthorized(res, token)
-      res.cookie('token', token, { httpOnly: true, secure: true });
-      const user = await this.service.getByEmail(req.body.email)
+      if (!token) httpResponse.Unauthorized(res, token);
+      res.cookie("token", token, { httpOnly: true, secure: true });
+      const user = await this.service.getByEmail(req.body.email);
       const { firstName, lastName, email, age, role } = user;
       res.json({
-        msg: 'Successfully logged in',
+        msg: "Successfully logged in",
         user: {
           firstName,
-          lastName, 
+          lastName,
           email,
           age,
-          role
+          role,
         },
         token,
       });
@@ -46,27 +47,45 @@ export default class UserController extends Controller {
     }
   };
 
-  profile = async(req, res, next)=>{
+  profile = async (req, res, next) => {
     try {
-     if(req.user){
-      const { _id } = req.user;
-      const user = await this.service.getUserById(_id);
-      httpResponse.Ok(res, user);
-     } else httpResponse.Unauthorized(res, user)
+      if (req.user) {
+        const { _id } = req.user;
+        const user = await this.service.getUserById(_id);
+        httpResponse.Ok(res, user);
+      } else httpResponse.Unauthorized(res, user);
     } catch (error) {
       next(error);
     }
   };
 
-  generateResetPass = async(req, res, next) => {
+  generateResetPass = async (req, res, next) => {
     try {
       const user = req.user;
       const token = await this.service.generateResetPass(user);
-      if(token){
-        await sendMail
-      }
+      if (token) {
+        await sendMail(user, "resetPass", token);
+        res.cookie("tokenpass", token);
+        httpResponse.Ok(res, 'Email "reset pass" sent ok');
+      } else httpResponse.BadRequest(res, 'Error sending email "reset pass"');
     } catch (error) {
-      
+      next(error);
+    }
+  };
+
+  async updatePass(req, res, next) {
+    try {
+      const user = req.user;
+      const { password } = req.body;
+      const { tokenpass } = req.cookies;
+      if (!tokenpass) return httpResponse.Unauthorized(res, token);
+      const updPass = await userService.updatePass(password, user);
+      if (!updPass)
+        return httpResponse.BadRequest(res, "Can't be the same password");
+      res.clearCookie("tokenpass");
+      return httpResponse.Ok(res, updPass);
+    } catch (error) {
+      next(error);
     }
   }
 }
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,13 +3,17 @@ import { checkAuth } from "../middlewares/authJwt.js";
 import passport from "passport";
 const router = Router();
 import UserController from "../controllers/userController.js";
-const userController = new UserController();
+const controller = new UserController();
 
-router.get("/login", passport.authenticate("login", { session: false }), userController.loginResponse);
+router.get("/login", passport.authenticate("login", { session: false }), controller.loginResponse);
 
-router.get("/register", passport.authenticate("register", { session: false }), userController.registerResponse);
+router.get("/register", passport.authenticate("register", { session: false }), controller.registerResponse);
 
-router.get("/current", checkAuth, userController.profile);
+router.get("/current", checkAuth, controller.profile);
+
+router.post('/reset-pass', checkAuth, controller.generateResetPass);
+
+router.put('/new-pass', checkAuth, controller.updatePass);
 
 router.post("/logout", (req, res) => {
   res.clearCookie("token");
